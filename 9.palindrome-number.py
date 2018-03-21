c50d1127214f97373cc676f2464bac95665de6cd
Sengege <trace_tristan@126.com>
2018-03-21 13:19:15 +0800

    palindrome-number

--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -10,23 +10,23 @@
 # Testcase Example:  '-2147483648'
 #
 # Determine whether an integer is a palindrome. Do this without extra space.
-# 
+#
 # click to show spoilers.
-# 
+#
 # Some hints:
-# 
+#
 # Could negative integers be palindromes? (ie, -1)
-# 
+#
 # If you are thinking of converting the integer to string, note the restriction
 # of using extra space.
-# 
+#
 # You could also try reversing an integer. However, if you have solved the
 # problem "Reverse Integer", you know that the reversed integer might overflow.
 # How would you handle such case?
-# 
+#
 # There is a more generic way of solving this problem.
-# 
-# 
+#
+#
 #
 class Solution(object):
     def isPalindrome(self, x):
@@ -34,4 +34,13 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        
+        if x<0:
+            return False
+        tmp = str(abs(x))
+        i , j = 0, len(tmp)-1
+        while i < j:
+            if tmp[i] != tmp[j]:
+                return False
+            i+=1
+            j-=1
+        return True
